a5595d02b20ce360c284e03ca10fda13ef5afef4
SoreMelrose <qckong23@163.com>
2020-04-13 22:28:50 +0800

    4.13运动页面接口连接

--- _tuyou/pages/xiaoyuanxiangqing/xiaoyuanxiangqing.js
+++ _tuyou/pages/xiaoyuanxiangqing/xiaoyuanxiangqing.js
@@ -3,7 +3,12 @@ Page({
   data: {
     CustomBar: app.globalData.CustomBar,
     TabCur: 0,
-    paimingCur:0
+    paimingCur:0,
+    categoryId:'',
+    detail:[],//页面详细内容
+    comment:[],
+    news:[],
+    news_detail:[]
   },
   tabSelect(e) {
     console.log(e);
@@ -29,22 +34,81 @@ Page({
       url: '/pages/chakanhuifu/chakanhuifu',
     })
   },
-  onLoad: function (options) {
-
+  detail() {//页面项目信息
+    let url = app.globalData.URL + '/act/findCampusActivity';
+    let data = {
+      id: this.data.categoryId
+    };
+    app.wxRequest('GET', url, data, (res) => {
+      this.setData({
+        detail: res.data
+      })
+    }, (err) => {
+      console.log(err.errMsg)
+    });
+  },
+  comment() {//评论
+    let url = app.globalData.URL + '/comm/listCommByObj';
+    let data = {
+      objid: this.data.categoryId,
+      objtype: 30
+    };
+    app.wxRequest('GET', url, data, (res) => {
+      this.setData({
+        comment: res.data
+      })
+    }, (err) => {
+      console.log(err.errMsg)
+    });
+  },
+  news() {//活动新闻
+    let url = app.globalData.URL + '/news/listNewsByAct';
+    let data = {
+      actid: this.data.categoryId
+    };
+    app.wxRequest('GET', url, data, (res) => {
+      this.setData({
+        news: res.data
+      })
+    }, (err) => {
+      console.log(err.errMsg)
+    });
+  },
+  news_detail() {//活动新闻
+    let url = app.globalData.URL + '/news/findNewsDetail';
+    let data = {
+      id: this.data.news.id
+    };
+    app.wxRequest('GET', url, data, (res) => {
+      this.setData({
+        news_detail: res.data
+      })
+    }, (err) => {
+      console.log(err.errMsg)
+    });
+  },
+  onLoad: function (options) {//读取活动对应id
+    this.setData({
+      categoryId: options.categoryId
+    })
+    this.detail()
+    this.comment()
+    this.news()
+   
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.news_detail()
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.news_detail()
   },
 
   /**
