dbec7ec41d3a437e156ea18b127ea318e7ba5861
SoreMelrose <qckong23@163.com>
2020-04-15 00:04:03 +0800

    4.15发布页面接口连接

--- _tuyou/pages/form_launch/form_launch.js
+++ _tuyou/pages/form_launch/form_launch.js
@@ -3,32 +3,39 @@ Page({
   data: {
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
-    index: null,
-    name: '',
+    index: 0,
+    index2: 0,
+    actname: '',
+    slogan:'',
+    place:'',
+    univid:'',
+    province:'',
+    city:'',
+    address:'',
+    venue:'',
+    creater:'',
+    createralias:'',
+    ischecked:false,
+    ischecked2: true,
     provinceList:[],
     city:[],
+    entrylimit:50,
     ActList:[],
     picker: ['个人报名', '团体报名'],
-    picker2: ['匿名参赛', '实名参赛'],
-    picker3: ['观看无需报名', '匿名报名观看', '实名报名观看'],
-    picker4: ['篮球', '足球', '排球', '羽毛球', '乒乓球', '其他'],
+    picker2: ['篮球', '足球', '羽毛球', '乒乓球', '网球'],
     multiIndex: [0, 0, 0],
     time: '12:01',
-    date: '2020.4.25 16:00',
-    date2: '2020.8.25 16:00',
+    fromtime: '2020.4.25 16:00',
+    signupdeadline: '2020.8.25 16:00',
     region: ['浙江省', '杭州市', '浙江大学'],
-    place: ['浙江大学篮球场'],
-    imgList: [],
-    modalName: null,
-    textareaAValue: '',
-    textareaBValue: ''
   },
 
   PickerChange(e) {
     console.log(e);
     this.setData({
       index: e.detail.value
-    })
+    });
+    console.log(this.data.picker2[this.data.index])
   },
   MultiChange(e) {
     this.setData({
@@ -42,7 +49,12 @@ Page({
   },
   DateChange(e) {
     this.setData({
-      date: e.detail.value
+      fromtime: e.detail.value
+    })
+  },
+  DateChange2(e) {
+    this.setData({
+      signupdeadline: e.detail.value
     })
   },
   RegionChange: function (e) {
@@ -50,46 +62,7 @@ Page({
       region: e.detail.value
     })
   },
-  ChooseImage() {
-    wx.chooseImage({
-      count: 4, //默认9
-      sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album'], //从相册选择
-      success: (res) => {
-        if (this.data.imgList.length != 0) {
-          this.setData({
-            imgList: this.data.imgList.concat(res.tempFilePaths)
-          })
-        } else {
-          this.setData({
-            imgList: res.tempFilePaths
-          })
-        }
-      }
-    });
-  },
-  ViewImage(e) {
-    wx.previewImage({
-      urls: this.data.imgList,
-      current: e.currentTarget.dataset.url
-    });
-  },
-  DelImg(e) {
-    wx.showModal({
-      title: '召唤师',
-      content: '确定要删除这段回忆吗？',
-      cancelText: '再看看',
-      confirmText: '再见',
-      success: res => {
-        if (res.confirm) {
-          this.data.imgList.splice(e.currentTarget.dataset.index, 1);
-          this.setData({
-            imgList: this.data.imgList
-          })
-        }
-      }
-    })
-  },
+ 
   textareaAInput(e) {
     this.setData({
       textareaAValue: e.detail.value
@@ -100,9 +73,35 @@ Page({
       textareaBValue: e.detail.value
     })
   },
-  name(e) {
+  getname(e) {
     this.setData({
-      name: e.detail.value
+      actname: e.detail.value
+    })
+  },
+  getslogan(e){
+    this.setData({
+      slogan: e.detail.value
+    })
+  },
+  getvenue(e){
+    this.setData({
+      venue: e.detail.value
+    })
+  },
+  getentity(e){
+    this.setData({
+      entrylimit: e.detail.value
+    })
+  },
+  changeswitch(e){    
+    this.setData({
+      ischecked: !this.data.ischecked
+    })
+  },
+  checked(e){
+    console.log(e)
+    this.setData({
+      ischecked2: !this.data.ischecked2
     })
   },
   cancel: function (e) {
@@ -110,12 +109,15 @@ Page({
       url: "../../pages/form/form"
     })
   },
+
   commit: function (e) {
-    console.log(e.detail.value)
+    let name = this.data.picker2[this.data.index]
+    console.log(name)
     wx.navigateTo({
-    url: "../../pages/form_activity/form_activity"
+    url: '../../pages/form_activity/form_activity'
     })
   },
+  
   city() {
     let url = app.globalData.URL + '/config/getCity';
     let data = {
