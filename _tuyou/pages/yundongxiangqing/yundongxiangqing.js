063b37277a57f387cc73806db72cbc18f015cbc8
wodilaoge <2645189252@qq.com>
2020-04-16 16:50:25 +0800

    新建活动详情

---
+++ _tuyou/pages/yundongxiangqing/yundongxiangqing.js
@@ -0,0 +1,77 @@
+// pages/yundongxiangqing/yundongxiangqing.js
+Page({
+  data: {
+    TabCur: 0,
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: '/img/yundongxiangqing.png'
+    }, {
+      id: 1,
+      type: 'image',
+        url: '/img/yundongxiangqing.png',
+    }],
+  },
+  tabSelect(e) {
+    console.log(e);
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
