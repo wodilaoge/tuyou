b0189e51d757ca15ec04358249ebdf741037c9ad
RJZJL <1158455621@qq.com>
2020-04-24 23:41:12 +0800

    团队信息接口+跳转

--- _tuyou/pages/tuanduixinxi/tuanduixinxi.js
+++ _tuyou/pages/tuanduixinxi/tuanduixinxi.js
@@ -1,18 +1,63 @@
 // pages/tuanduixinxi/tuanduixinxi.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    tdxxId:'6793995262885888',
+    tdxxDeatil:[],
+    duiyuanID:''
 
   },
+
+
+
+  getXinxi() {
+    let url = app.globalData.URL + '/team/findTeam';
+    let data = {
+      id:this.data.tdxxId
+    };
+    app.wxRequest('GET', url, data, (res) => {
+      console.log(res.data)
+      this.setData({
+      tdxxDeatil:res.data
+      })
+    }, (err) => {
+      console.log(err.errMsg)
+    });
+  },
+
+  navziliao: function (e) {
+    console.log(e)
+    wx.navigateTo({
+      url: '/pages/ziliao/ziliao'
+    })
+  },
+
+  test: function () {
+    let url = app.globalData.URL + '/team/listMember';
+    let data = {
+      id: this.data.tdxxId
+    };
+    app.wxRequest('GET', url, data, (res) => {
+      console.log(res.data)
+      this.setData({
+       
+      })
+    }, (err) => {
+      console.log(err.errMsg)
+    });
+  },
+   
+  
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getXinxi()
   },
 
   /**
