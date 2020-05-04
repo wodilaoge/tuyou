698e08b3263fc5c59d7ce61d45d396a8672c88c1
SoreMelrose <qckong23@163.com>
2020-05-04 22:10:26 +0800

    add some new pages

--- _tuyou/pages/MyPages/my_interest/my_interest.js
+++ _tuyou/pages/MyPages/my_interest/my_interest.js
@@ -1,9 +1,5 @@
 // pages/MyPages/my_interest/my_interest.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     lists: [{
         'title': '篮球',
@@ -39,59 +35,7 @@ Page({
       [temp]:!this.data.lists[t].is
     })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function(options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
 })
