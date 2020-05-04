b60183c407b229a946ec2dbe9e02c4bd75a7d774
SoreMelrose <qckong23@163.com>
2020-05-04 21:04:38 +0800

    add many pages

---
+++ _tuyou/pages/MyPages/my_interest/my_interest.js
@@ -0,0 +1,87 @@
+// pages/MyPages/my_interest/my_interest.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    lists:[{
+      'title':'篮球',
+      'content':'推荐篮球内容'
+    },
+      {
+        'title': '篮球',
+        'content': '推荐篮球内容'
+      },
+      {
+        'title': '篮球',
+        'content': '推荐篮球内容'
+      },
+      {
+        'title': '篮球',
+        'content': '推荐篮球内容'
+      },
+      {
+        'title': '篮球',
+        'content': '推荐篮球内容'
+      }]
+  },
+  choose(e){
+    console.log(e.currentTarget.dataset.id)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- _tuyou/pages/MyPages/my_rank/my_rank.js
+++ _tuyou/pages/MyPages/my_rank/my_rank.js
@@ -11,7 +11,36 @@ Page({
     options: 1,
     TabCur: 0,
     AllActivity: [],
-    scrollLeft: 0
+    scrollLeft: 0,
+    messages: [{
+        'union':'XXX',
+        'rank': 1,
+        'score': 80,
+        'date': '2020/3/31',
+        'name': 'XXX活动名称'
+      },
+      {
+        'union': 'XXX',
+        'rank': 1,
+        'score': 80,
+        'date': '2020/3/31',
+        'name': 'XXX活动名称'
+      },
+      {
+        'union': 'XXX',
+        'rank': 1,
+        'score': 80,
+        'date': '2020/3/31',
+        'name': 'XXX活动名称'
+      },
+      {
+        'union': 'XXX',
+        'rank': 1,
+        'score': 80,
+        'date': '2020/3/31',
+        'name': 'XXX活动名称'
+      }
+    ]
   },
   changebutton(e) {
     this.setData({
@@ -24,11 +53,11 @@ Page({
       scrollLeft: (e.currentTarget.dataset.id - 1) * 60
     })
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this
     let url = app.globalData.URL + '/config/findAllActivityClass1';
     let data = '';
-    util.gets(url, {}).then(function (res) {
+    util.gets(url, {}).then(function(res) {
       that.setData({
         AllActivity: res.data.data
       })
@@ -38,49 +67,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
