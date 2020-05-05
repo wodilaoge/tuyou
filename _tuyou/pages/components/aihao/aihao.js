e66b28c272b5f920c9dc6dded9a5319b0be0c079
wodilaoge <2645189252@qq.com>
2020-05-05 22:41:55 +0800

    爱好文娱

---
+++ _tuyou/pages/components/aihao/aihao.js
@@ -0,0 +1,26 @@
+// pages/components/aihao/aihao.js
+Component({
+  options: {
+    addGlobalClass: true
+  },
+  properties: {
+    aihaoList: Object,
+    aihaoCur: String
+  },
+  attached: function () {
+    console.log(this.properties.yundongCur);
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
