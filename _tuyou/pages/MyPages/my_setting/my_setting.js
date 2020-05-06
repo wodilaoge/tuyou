93a6f44174ef8c9c8afbf1d656b5ca9bd1d08a16
SoreMelrose <qckong23@163.com>
2020-05-06 17:27:06 +0800

    finished my page

--- _tuyou/pages/MyPages/my_setting/my_setting.js
+++ _tuyou/pages/MyPages/my_setting/my_setting.js
@@ -5,14 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    time: '12:01',
   },
-
+  TimeChange(e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
+  },
+  tomessage(){
+    wx.navigateTo({
+      url: '/pages/MyPages/my_messagemind/my_messagemind',
+    })
   },
   toblack(){
     wx.navigateTo({
