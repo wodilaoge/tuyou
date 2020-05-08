1eca671a8ccc25b3942d2641632eb1cad95e1379
SoreMelrose <qckong23@163.com>
2020-05-08 19:09:53 +0800

    Update my_follow.js

--- _tuyou/pages/MyPages/my_follow/my_follow.js
+++ _tuyou/pages/MyPages/my_follow/my_follow.js
@@ -1,7 +1,10 @@
+const app = getApp();
+var util = require("../../../utils/util.js");
 Page({
 
   data: {
     TabCur:0,
+    infoall:[],
     lists: [{
         'name': '朋友',
         'num': 8
@@ -54,6 +57,17 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    var that = this
+    let url = app.globalData.URL + '/follow/listFollowByUserType';
+    let t=wx.getStorageSync('userinfo')
+    let data={
+      'uid':t.id,
+      'objtype':30
+    }
+    util.gets(url, data).then(function (res) {
+      that.setData({
+        infoall: res.data.data
+      })
+    })
   },
 })
