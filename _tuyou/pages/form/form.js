2038f0f19c06f5d60ca6d394c5f8a411767242c2
SoreMelrose <qckong23@163.com>
2020-05-10 16:31:23 +0800

    发布页面修改

---
+++ _tuyou/pages/form/form.js
@@ -0,0 +1,36 @@
+// pages/form/form.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  toact: function (e) {
+    wx.navigateTo({
+      url: "../../pages/form_launch/form_launch"
+    })
+  },
+  tovideo: function (e) {
+    wx.navigateTo({
+      url: "../../pages/form_video/form_video"
+    })
+  },
+  topic: function (e) {
+    wx.navigateTo({
+      url: "../../pages/form_picture/form_picture"
+    })
+  },
+  toteam: function (e) {
+    wx.navigateTo({
+      url: "../../pages/form_team/form_team"
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+})
