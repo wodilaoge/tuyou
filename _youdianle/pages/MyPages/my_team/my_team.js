31923855c0a8dcc8c03c9f4bcf84931dc5cfae1b
SoreMelrose <qckong23@163.com>
2020-05-13 18:32:17 +0800

    页面修复

--- _youdianle/pages/MyPages/my_team/my_team.js
+++ _youdianle/pages/MyPages/my_team/my_team.js
@@ -1,59 +1,14 @@
-const app = getApp()
-const util = require("../../../utils/util.js");
+const app = getApp();
 Page({
   data: {
-    StatusBar: app.globalData.StatusBar,
-    CustomBar: app.globalData.CustomBar,
-    Custom: app.globalData.Custom,
     TabCur: 0,
-    MainCur: 0,
     VerticalNavTop: 0,
-    options: 1,
-    AllActivity: [],
+    MainCur: 0,
+    YundongList: app.globalData.YundongList,
     list: [],
-    load: true
-  },
-  select1() {
-    this.setData({
-      options: 1
-    })
-  },
-  select2() {
-    this.setData({
-      options: 2
-    })
-  },
-  select3() {
-    this.setData({
-      options: 3
-    })
-  },
-  onLoad() {
-    wx.showLoading({
-      title: '加载中...',
-      mask: true
-    });
-    let list = [{}];
-    for (let i = 0; i < 26; i++) {
-      list[i] = {};
-      list[i].name = String.fromCharCode(65 + i);
-      list[i].id = i;
-    }
-    this.setData({
-      list: list,
-      listCur: list[0]
-    })
-    var that = this
-    let url = app.globalData.URL + '/config/findAllActivityClass1';
-    let data = '';
-    util.gets(url, {}).then(function (res) {
-      that.setData({
-        AllActivity: res.data.data
-      })
-    })
-  },
-  onReady() {
-    wx.hideLoading()
+    activitylist:[],
+    teams: [],
+    lid: '',
   },
   tabSelect(e) {
     this.setData({
@@ -62,13 +17,30 @@ Page({
       VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
     })
   },
+  tabSelectTeam(e) {
+    console.log(e.currentTarget.dataset.item)
+    var pages = getCurrentPages();
+    var prevPage = pages[pages.length - 2];  //上一个页面
+    prevPage.setData({
+      tuanduiSelect: e.currentTarget.dataset.item
+    });
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  todetail(e){
+    console.log(e.currentTarget.dataset.item)
+    wx.navigateTo({
+      url: '/pages/MyPages/my_team_detail/my_team_detail?id=' + e.currentTarget.dataset.item.id,
+    })
+  },
   VerticalMain(e) {
-    let that = this;
-    let list = this.data.AllActivity;
+    let self = this;
+    let list = this.data.list;
     let tabHeight = 0;
     if (this.data.load) {
       for (let i = 0; i < list.length; i++) {
-        let view = wx.createSelectorQuery().select("#main-" + list[i].code);
+        let view = wx.createSelectorQuery().select("#main-" + list[i].id);
         view.fields({
           size: true
         }, data => {
@@ -77,7 +49,7 @@ Page({
           list[i].bottom = tabHeight;
         }).exec();
       }
-      that.setData({
+      self.setData({
         load: false,
         list: list
       })
@@ -85,12 +57,65 @@ Page({
     let scrollTop = e.detail.scrollTop + 20;
     for (let i = 0; i < list.length; i++) {
       if (scrollTop > list[i].top && scrollTop < list[i].bottom) {
-        that.setData({
+        self.setData({
           VerticalNavTop: (list[i].id - 1) * 50,
           TabCur: list[i].id
         })
         return false
       }
     }
-  }
+  },
+  getteams() {
+    let t=wx.getStorageSync('userinfo')
+    var self = this
+    let url = app.globalData.URL + '/team/listByLeader'
+    let data = {
+      lid: '11878394752925696'
+    }
+    app.wxRequest('GET', url, data, (res) => {
+      console.log(res.data)
+      self.setData({
+        teams: res.data
+      })
+    }, (err) => {
+      console.log(err.errMsg)
+    });
+    let url2 = app.globalData.URL + '/team/listAcid1ByLeader'
+    let data2 = {
+      lid: '11878394752925696'
+    }
+    app.wxRequest('GET', url2, data2, (res) => {
+      console.log(res.data)
+      self.setData({
+        activitylist: res.data
+      })
+    }, (err) => {
+      console.log(err.errMsg)
+    });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let self = this;
+    // wx.showLoading({
+    //   title: '加载中...',
+    //   mask: true
+    // });
+    self.setData({
+      lid: options.lid
+    })
+    self.getteams()
+    let list = [{}];
+    for (let i in this.data.YundongList) {
+      list[i] = {};
+      list[i].name = this.data.YundongList[i].name;
+      list[i].id = i;
+    }
+    this.setData({
+      list: list,
+      listCur: list[0]
+    })
+  },
+
 })
