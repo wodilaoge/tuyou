70e5e6aed402a26f5d8088248ac7b19df8b9ec69
SoreMelrose <qckong23@163.com>
2020-09-15 20:59:14 +0800

    9.15

--- _youdianle/pages/MyPages/my/my.js
+++ _youdianle/pages/MyPages/my/my.js
@@ -37,7 +37,7 @@ Page({
   },
   ChooseImage() {
     wx.chooseImage({
-      count: 4, //默认9
+      count: 1, //默认9
       sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album'], //从相册选择
       success: (res) => {
--- _youdianle/pages/form_picture/form_picture.js
+++ _youdianle/pages/form_picture/form_picture.js
@@ -108,7 +108,7 @@ Page({
     var that = this
     var t = e.currentTarget.dataset.id
     wx.chooseImage({
-      count: 9, //默认9
+      count: 1, //默认9
       sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album'], //从相册选择
       success: (res) => {
